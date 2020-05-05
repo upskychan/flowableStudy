cf5b52ca667a7cce3f13e89f2ad95fe3f91af727
chenjiel <chenjiel@yonyou.com>
2020-05-05 21:08:01 +0800

    使用slf4j+logback输出日志

--- flowable-study-engine/src/test/java/com/upsky/flowablestudy/history/HistoryTest.java
+++ flowable-study-engine/src/test/java/com/upsky/flowablestudy/history/HistoryTest.java
@@ -2,6 +2,8 @@ package com.upsky.flowablestudy.history;
 
 import com.upsky.flowablestudy.variable.Person;
 import org.flowable.engine.*;
+import org.flowable.engine.history.HistoricActivityInstance;
+import org.flowable.engine.history.HistoricProcessInstance;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.DeploymentBuilder;
 import org.flowable.engine.runtime.ProcessInstance;
@@ -11,6 +13,8 @@ import org.flowable.variable.api.history.HistoricVariableInstance;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -25,6 +29,8 @@ import java.util.Map;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:flowable-context.xml")
 public class HistoryTest {
+    private Logger logger = LoggerFactory.getLogger(HistoryTest.class);
+
     private ProcessEngine processEngine;
     private RepositoryService repositoryService;
     private RuntimeService runtimeService;
@@ -67,7 +73,7 @@ public class HistoryTest {
     public void deploy() {
         DeploymentBuilder deploymentBuilder = repositoryService.createDeployment().category("历史数据查询相关的测试").name("historyTest").key("dKey-variable").addClasspathResource("bpmn_xml/历史数据查询.bpmn20.xml");
         Deployment deploy = deploymentBuilder.deploy();
-        System.out.println(deploy.getId());
+        logger.info("deloyId:{}", deploy.getId());
     }
 
     /**
@@ -78,12 +84,46 @@ public class HistoryTest {
         String processDefinitionKey = "history";
         String businessKey = "bKey-history";
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey, businessKey);
-        System.out.println(processInstance.getId() + "," + processInstance.getActivityId());
+        logger.info(processInstance.getId() + "," + processInstance.getActivityId());
+        String procInstId = "167501";
     }
 
+    /**
+     * 查询已完成流程实例
+     * SQL:select distinct RES.* , DEF.KEY_ as PROC_DEF_KEY_, DEF.NAME_ as PROC_DEF_NAME_, DEF.VERSION_ as PROC_DEF_VERSION_, DEF.DEPLOYMENT_ID_ as DEPLOYMENT_ID_
+     * from ACT_HI_PROCINST RES left outer join ACT_RE_PROCDEF DEF on RES.PROC_DEF_ID_ = DEF.ID_ WHERE RES.END_TIME_ is not NULL order by RES.ID_ asc
+     */
     @Test
-    public void test(){
-        String procInstId = "167501";
+    public void testQueryHistoricProcessInstance() {
+        List<HistoricProcessInstance> hisProcInstList = historyService.createHistoricProcessInstanceQuery().finished().list();
+        if (hisProcInstList != null) {
+            for (HistoricProcessInstance hisProcInst : hisProcInstList) {
+                logger.info(hisProcInst.getId() + ":" + hisProcInst.getName() + ":" + hisProcInst.getBusinessKey() + ":" + hisProcInst.getProcessDefinitionKey() + ":" + hisProcInst.getDurationInMillis());
+            }
+        }
+    }
+
+    /**
+     * 查询历史流程活动
+     * SQL:select RES.* from ACT_HI_ACTINST RES WHERE RES.END_TIME_ is not null order by RES.ID_ asc
+     */
+    @Test
+    public void testHistoricActivityInstanceQuery() {
+        List<HistoricActivityInstance> hisActInstList = historyService.createHistoricActivityInstanceQuery().finished().list();
+        if (hisActInstList != null) {
+            String lastProcInstId = null;
+            for (HistoricActivityInstance hisActInst : hisActInstList) {
+                String procInstId = hisActInst.getProcessInstanceId();
+                if (!procInstId.equals(lastProcInstId)) {
+                    lastProcInstId = procInstId;
+                    logger.info("****************流程实例{}**************", hisActInst.getProcessInstanceId());
+                }
+                logger.info("活动节点ID：{}", hisActInst.getActivityId());
+                logger.info("活动节点名称：{}", hisActInst.getActivityName());
+                logger.info("活动节点类型：{}", hisActInst.getActivityType());
+                logger.info("操作人：{}", hisActInst.getAssignee());
+            }
+        }
     }
 
 }
